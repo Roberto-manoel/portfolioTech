b13aff5147c5c0916f98597051e3087b6890efeb
Roberto-Manuel <roberto.programadortech@gmail.com>
2023-12-21 08:36:45 -0300

    adicionando projetos e bootstrap.

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from pages import views
+from pages import views as pages_views
+from projects import views as projects_views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-
+    path('', pages_views.home, name='home'),
+    path("", projects_views.project_index, name="project_index"),
+    path("<int:pk>/", projects_views.project_detail, name="project_detail"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from projects import views
 
-# Create your views here.
 def home(request):
     return render(request, 'pages/home.html', {})
+
+def project_index(request):
+    projects = Project.objects.all()
+    context = {
+        "projects": projects
+}
+    return render(request, "projects/project_index.html", context)
+
+def project_detail(request, pk):
+    project = Project.objects.get(pk=pk)
+    context = {
+        "project": project
+}
+    return render(request, "projects/project_detail.html", context)
